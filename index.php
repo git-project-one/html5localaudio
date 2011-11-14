65d1209e1d431967df6d03a9a4dfc2035c369fb9
unknown <abashkhaz@CI0000001513972.rim.net>
2011-11-14 18:19:02 -0500

    Added: JS controls for Track Playback/Shuffling

--- index.php
+++ index.php
@@ -6,12 +6,18 @@
 	</head>
 	<body>
 		<div id="playerdiv">
-			<div id="controls">
-				<div id="prevBtn" class="btn"></div>
-				<div id="playBtn" class="btn"></div>
-				<div id="nextBtn" class="btn"></div>
+			<div class="meta">
+				<div id="controls" style="float:left">
+					<div id="prevBtn" class="btn"></div>
+					<div id="playBtn" class="btn"></div>
+					<div id="nextBtn" class="btn"></div>
+				</div>
+				<div id="titleSpan">Title</div>
+
+				<div class="progressBar" style="float:right">
+					<span id ="currentTime" style="float:left">00:00</span><div id="bar"><div id="handle"></div></div><span id ="totalTime">00:00</span>
+				</div>
 			</div>
-			
 			<div id="display">
 				<div class="container window">
 						<div style="border-right:1px solid rgba(173, 58, 211, 0.4)"></div>
@@ -19,13 +25,17 @@
 				</div>		
 			</div>
 			
-			<div class="progressBar">
-				<span id ="currentTime" style="float:left">00:00</span><div id="bar"><div id="handle"></div></div><span id ="totalTime">00:00</span>
-			</div>
+			
+			
+			<div id="playlist">
 			<!-- The actual music player business. -->
 <?php
 		/* Pull in all of the variables off of the url string and parse out any URL encoding. */
-		$dir=($_GET['dir']);
+		//$root = "player";
+		require_once('getid3/getid3.php');
+		$getID3 = new getID3;
+		
+		$dir= "mp3".($_GET['dir']);
 		$dir=(urldecode($dir));
 		$play=($_GET['play']);
 		$play=(urldecode($play));
@@ -49,9 +59,12 @@ $count=0;
 $array=array();
 	
 	/* load in a directory and read in the list of files contained in it */
-	if ($handle = opendir("/var/www/$dir")) {
+	//if ($handle = opendir("/var/www/$dir")) {
+	if ($handle = opendir("$dir")) {
+
 	    echo "<a href='/'>Go Home</a> || <a href='/?dir=$parent'>Go Up a level</a><br /><br />";
 	    echo "<b>Songs:</b> <br />";
+		echo '<ul id="list">';
 	  /*  echo "DIR IS AS FOLLOWS: $dir <br />"; */
 	    
 	    while (false !== ($file = readdir($handle))) {
@@ -59,8 +72,21 @@ $array=array();
 	
 		
 		if (preg_match("/mp3$/", "$file")){
+			// Analyze file and store returned data in $ThisFileInfo
+			$ThisFileInfo = $getID3->analyze($pathfromroot);
+			getid3_lib::CopyTagsToComments($ThisFileInfo);
+			
+			$artist = $ThisFileInfo['comments_html']['artist'][0];
+			$title = $ThisFileInfo['tags']['id3v2']['title'][0];
+						
 			/* This block is used for handling links that end in .mp3 */
-			echo "<a href='/?dir=$dir&play=$pathfromroot'>$file</a><br />";
+			//echo "<a href='/player/?dir=$dir&play=$pathfromroot'>$file</a><br />";
+			//if ID3 tags are empty - display file name
+			if($artist == '' OR $title == '')
+				echo "<li data-dir='".$dir."' data-filename='".$file."' data-id= '".$count."' data-artist='".$artist."' data-title='".$title."' data-url='$pathfromroot'>".$file."</a></li>";
+			else
+				echo "<li data-dir='".$dir."' data-filename='".$file."' data-id= '".$count."' data-artist='".$artist."' data-title='".$title."' data-url='$pathfromroot'>".$artist." - ".$title."</a></li>";
+
 			$array[$count] = "/?dir=$dir&play=$pathfromroot";
 			$count=$count+1;
 		}
@@ -72,19 +98,14 @@ $array=array();
 				}
 			}
 		}
-
+		echo '</ul>';
     		closedir($handle);
 	}
 	/* Motherfucking cocksucking christ, I spent like 30 minutes learning that one should not code after 2 am. 'While' loops how do they work??!?!?!?! */
-	shuffle($array);
+	//shuffle($array);
 	/* <!-- This script is used to pull the status of the ending of a song. --> */
 	?>
-		<script>
-			var audio = document.getElementsByTagName('audio')[0];
-			audio.addEventListener('ended', function () {
-			window.location='<?php echo"$array[0]"; ?>'
-			} );
-		</script>
+	</div>
 	</div>
 
 	</body>
--- js/script.js
+++ js/script.js
@@ -6,8 +6,89 @@ window.addEventListener("load",function(){
 	var totalTimeSpan = document.getElementById('totalTime');
 	var bar = document.getElementById('bar');
 	var playBtn = document.getElementById('playBtn');
+	var titleSpan = document.getElementById('titleSpan');
+	var list = document.getElementById('list');
+	var currentTrack = 0;
+	var listArray = list.childNodes;
 	var playing = false;
 	var mousedown = false;
+	var http = new XMLHttpRequest();
+	
+		function shuffle(){
+		var index = Math.floor(Math.random()*listArray.length);
+		playTrack(listArray[index]);
+	}
+	
+
+	for (i=0;i<listArray.length;i++){
+		listArray[i].addEventListener("click",function(){
+			playTrack(this);
+		});
+	}
+	
+	prevBtn.addEventListener('click',function(){
+		if(parseInt(currentTrack)-1>=0)
+			playTrack(listArray[--currentTrack])
+	});
+	
+	nextBtn.addEventListener('click',function(){
+		if(parseInt(currentTrack)+1<listArray.length)
+			playTrack(listArray[++currentTrack])
+	});
+	
+	
+	
+	function post(mp3,dir){
+		var url = "convert.php";
+		var params = "mp3=" + mp3 + "&dirname=mp3&width=30000&height=110&foreground=#AAAAAA&background=";
+
+		http.open("POST", url, true);
+
+		//Send the proper header information along with the request
+		http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		//multipart/form-data
+	//	http.setRequestHeader("Content-length", params.length);
+	//	http.setRequestHeader("Connection", "close");
+
+		http.onreadystatechange = function() {//Call a function when the state changes.
+		
+			if(http.readyState == 4 && http.status == 200) {
+				
+				console.log(http.responseText);
+				display.style.backgroundImage = "url("+http.responseText+")";
+				player.play();
+			}
+		}
+		http.send(params);
+	}
+
+	
+	function playTrack(obj){
+	
+	currentTrack = obj.getAttribute("data-id");
+	var title = obj.getAttribute("data-title");
+	var artist = obj.getAttribute("data-artist");
+	var url = obj.getAttribute("data-url");
+	var dir = obj.getAttribute("data-dir");
+	var filename = obj.getAttribute("data-filename");
+	
+	//post(filename,dir);
+	
+	if(artist == '' || title == '')
+		titleSpan.innerText = url;
+	else
+		titleSpan.innerText = artist + " - " + title;
+	
+	setSource(url);
+	playBtn.style.backgroundImage = "url(/player/img/pause.png)";
+	player.play();
+	}
+
+	
+	player.addEventListener('ended',function(){
+	if(shuffle)
+		shuffle();
+	});
 	
 	
 	player.addEventListener('timeupdate',function(){
@@ -25,7 +106,9 @@ window.addEventListener("load",function(){
 		currentTimeSpan.innerText = time.m + ":" + time.s;
 	}
 
-	player.addEventListener('play',function(){
+	player.addEventListener('loadedmetadata',function(){
+		playing=true;
+//		titleSpan.innerText = this.getAttribute("alt");
 		totalTimeSpan.innerText = secondsToTime(player.duration).m + ":" + secondsToTime(player.duration).s;
 	});
 	
@@ -33,14 +116,15 @@ window.addEventListener("load",function(){
 		if(playing){
 			player.pause();
 			playing = false;
-			this.style.backgroundImage = "url(/musiverse/public/images/play.png)";
+			this.style.backgroundImage = "url(/player/img/play.png)";
 			}
 		else{
 			player.play();
 			playing = true;
-			this.style.backgroundImage = "url(/musiverse/public/images/pause.png)";
+			this.style.backgroundImage = "url(/player/img/pause.png)";
 		}
 	});
+	
 	
 	function updatePlayer(posComp){
 		display.style.backgroundPosition = posComp;
@@ -137,5 +221,40 @@ function secondsToTime(secs)
     };
     return obj;
 }
+function setSource(url) {
 	
+		// Fix for IE9 which can't set .src when there are <source> elements. Awesome, right?
+		var 
+			existingSources = player.getElementsByTagName('source');
+		while (existingSources.length > 0){
+			player.removeChild(existingSources[0]);
+		}
+	
+		if (typeof url == 'string') {
+			player.src = url;
+		} else {
+			var i, media;
+
+			for (i=0; i<url.length; i++) {
+				media = url[i];
+				if (player.canPlayType(media.type)) {
+					player.src = media.src;
+				}
+			}
+		}
+		//player.play();
+}
 });
+
+function findIndex(obj,value){
+var ctr = "";
+for (var i=0; i < obj.length; i++) {
+// use === to check for Matches. ie., identical (===), ;
+if (obj[i] === value) {
+return i;
+}
+}
+return ctr;
+};
+
+
